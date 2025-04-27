381697d12d32b3804c0941c6ef4a0fac1b661ffa
Dmytro Stehantsev <stegan296@gmail.com>
2025-04-27 20:09:44 +0300

    flake8

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -52,7 +52,8 @@ class MovieListSerializer(MovieSerializer):
     actors = serializers.SerializerMethodField()
 
     def get_actors(self, obj: Movie) -> list[str]:
-        return [f"{actor.first_name} {actor.last_name}" for actor in obj.actors.all()]
+        return [(f"{actor.first_name} "
+                 f"{actor.last_name}") for actor in obj.actors.all()]
 
 
 class MovieRetrieveSerializer(MovieSerializer):
